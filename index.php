229431dbaacc6a9fc3bd514146921458dd5e6430
AndreaMessene <amessene97@gmail.com>
2023-05-19 16:59:59 +0200

    stampa lunghezza e censura effettuate

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-    $password= $_GET['password']
+    
 
 
 ?>
--- stampa.php
+++ stampa.php
@@ -2,7 +2,7 @@
 
     $paragrafo= $_GET['paragrafo'];
     $censura= $_GET['censura'];
-    
+    $paragrafoCensurato= str_replace($censura, '****', $paragrafo);
 ?>
 
 
@@ -17,15 +17,25 @@
     <title>stampa</title>
 </head>
 <body>
-    
     <div>
-        <?php
-                echo $paragrafo;
-                echo $censura;
-        ?>
+        <?php echo $paragrafo ?>
+        <div>----------------</div>
+        <?php echo "il paragrafo è lungo:  " . strlen($paragrafo) ?>
+        <div>----------------</div>
+
+
     </div>
+    
+
+    <div>
+    <?php echo $paragrafoCensurato ?>
+    <div>-----------</div>
+    <?php echo "il paragrafo censurato è lungo: " . strlen($paragrafoCensurato) ?>
+
+    </div>
+
 
 
     
 </body>
-</html>
+</html> 
